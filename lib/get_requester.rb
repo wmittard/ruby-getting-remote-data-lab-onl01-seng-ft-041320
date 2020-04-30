3b9948941e23cc56b18d8700d237d7dbbee53580
William Mittard <billymittard@ymail.com>
2020-04-30 22:30:03 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,9 +3,14 @@ require 'open-uri'
 require 'json'
 
 class GetRequester 
+   
    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
    
- 
+ def get_response_body
+  uri = URI.parse(URL)
+  response = Net::HTTP.get_response(uri)
+  response.body
+end
   
   
   def parse_json
