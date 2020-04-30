ac8cd3340dd0b95748e536769c17a069d22c23ed
William Mittard <billymittard@ymail.com>
2020-04-30 22:29:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,11 +6,7 @@ class GetRequester
    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
    
  
-  def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
+  
   
   def parse_json
   end
