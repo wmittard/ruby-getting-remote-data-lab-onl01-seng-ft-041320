0cd9cf464d6073625b57b747cfab2468f06dbbdd
William Mittard <billymittard@ymail.com>
2020-04-30 22:25:51 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ require 'json'
 class GetRequester 
    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
    
-   def get_requester 
+   def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
