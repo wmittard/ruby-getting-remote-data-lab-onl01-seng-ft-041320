3912a69763d43a6dbc3adf6b528079b5cb668f06
William Mittard <billymittard@ymail.com>
2020-04-30 22:24:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,5 +1,13 @@
+
+
 class GetRequester 
    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
    
-   
+   def get_requester 
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+ 
+
 end
